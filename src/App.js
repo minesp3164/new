cf2db1bdb549d693d9816f6f6714a4b8501a86ec
minesp3164 <lhd000721@gmail.com>
2022-12-02 16:47:25 +0900

    add tailwindcss && replace App.js to TodoIndex.js and ignore .idea dir

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
-import React, { useCallback, useRef, useState } from "react";
-import TodoTemplate from "./components/Todo/TodoTemplate"
-import TodoInsert from "./components/Insert/TodoInsert";
-import TodoList from "./components/list/TodoList";
+import React from "react";
+import TodoIndex from './components/TodoRouter/TodoIndex';
+import Footer from './components/Footer/footer';
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "운동",
-      checked: true,
-  },
-    {
-      id: 2,
-      text: "공부",
-      checked: false,
-  },
-    {
-      id: 3,
-      text: "청소",
-      checked: true,
-  }
-]);
-const nextId = useRef(4);
 
-const onInsert = useCallback(
-  text =>{
-    const todo ={
-      id: nextId.current,
-      text,
-      checked: false,
-    };
-    setTodos(todos.concat(todo));
-    nextId.current += 1;
-  },
-  [todos],
-);
-const onRemove = useCallback(
-  id =>{
-    setTodos(todos.filter(todo => todo.id !== id));
-  },
-  [todos],
-)
-
-const onToggle = useCallback(
-  id =>{
-    setTodos(
-      todos.map(todo =>
-        todo.id === id ? {...todo,checked : !todo.checked}: todo),
-    );
-  },
-  [todos],
-);
   return (
-  <TodoTemplate>
-    <TodoInsert onInsert ={onInsert}/>
-    <TodoList todos = {todos} onRemove = {onRemove} onToggle ={onToggle}/>
-  </TodoTemplate>  
+    <div>
+        <TodoIndex/>
+      <Footer/>
+    </div>
   );
 };
 
